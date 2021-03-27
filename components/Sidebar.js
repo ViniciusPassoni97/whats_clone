e86c8e6be9adaa118308af147f113cd2e7354e1c
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-03-27 13:44:16 -0300

    Terminando header em Sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,14 +1,49 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import {Avatar,IconButton} from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChatIcon from '@material-ui/icons/Chat';
 
 export default function Sidebar() {
     return (
         <Container>
-            <h1>Sidebar</h1>
+            <Header>
+                <UserAvatar />
+                <IconsContainer>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </IconsContainer>
+            </Header>
         </Container>
     )
 }
 
+const Header = styled.div`
+    display:flex;
+    position:sticky;
+    background-color: white;
+    top:0;
+    z-index:1;
+    justify-content: space-between;
+    align-items: center;
+    padding:1.2rem;
+    height:5rem;
+    border-bottom: 1px solid whitesmoke;
+`;
 const Container = styled.div`
 
+`;
+const UserAvatar = styled(Avatar)`
+    cursor: pointer;
+
+    :hover {
+        opacity: 0.8;
+    }
+`;
+
+const IconsContainer = styled.div`
 `;
