12a745cdd71bed2469751bd812519d415f8e44d4
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-03-27 13:15:49 -0300

    Adiciona criação de Sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Sidebar() {
+    return (
+        <Container>
+            <h1>Sidebar</h1>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`;
